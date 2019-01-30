910ba60cdc9b4f508eeaeaec1b481ddee48aaf8c
Rohit Singhal <me.rohitsinghal@gmail.com>
2019-01-30 21:21:53 +0530

    Lens create dataset test cases & cicrleci config modified

--- lens/tests.py
+++ lens/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+
+import json
+from random import randint
 
 # Create your tests here.
+
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.id = randint(10, 100)
+        self.scsMsg = "Successfully Created dataset for id: {}".format(self.id)
+        self.missMsg = "userId Parameter Missing."
+        self.errorMsg = "Exception Occurred in create dataset!"
+
+
+class DataSetCreationTestCases(BaseViewTest):
+
+    def successfulDatasetCreationCase(self):
+        response = self.client.post(
+            reverse("create_dataset", kwargs={"version": "v1"}),
+            data=json.dump({'userId': self.id}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.data, self.scsMsg)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def ParameterMissingCase(self):
+        response = self.client.post(
+            reverse("create_dataset", kwargs={"version": "v1"}),
+            data=json.dump({}),
+            content_type='application/json'
+        )
+        self.assertEqual(response.data, self.missMsg)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- lens/views.py
+++ lens/views.py
@@ -1,38 +1,53 @@
-from django.shortcuts import render, redirect
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 import cv2
 
+from django.utils.datastructures import MultiValueDictKeyError
 from api.settings import BASE_DIR
 
-# Create your views here.
+
+def index():
+    return Response("Try me!")
 
 
+@api_view(['POST'])
 def create_dataset(request, version):
-    id = request.POST['userId']
-    print(cv2.__version__)
-    # Detect Face
-    # Creating a cascade Image classifier
-    faceDetect = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    # camture images from the webcam and process and detect the face
-    cam = cv2.VideoCapture(0)
+    print("OpenCv Version:", cv2.__version__)
+    resStatus = status.HTTP_201_CREATED
+    resMsg = "Successfully Created dataset for id: {}"
 
-    samples = 0
-    # Capturing the faces one by one and detect the faces and showing it on the window
-    while(True):
-        ret, img = cam.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-        for(x,y,w,h) in faces:
-            samples = samples + 1
-            # Saving the image dataset, only the face part
-            cv2.imwrite(BASE_DIR+'/ml/dataset/user.'+str(id)+'.'+str(samples)+'.jpg', gray[y:y+h,x:x+w])
-            cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
-            cv2.waitKey(250)
-        cv2.imshow("Face",img)
-        cv2.waitKey(1)
-        if(samples>5):
-            break
-
-    cam.release()
-    cv2.destroyAllWindows()
-
-    return redirect('/')
+    try:
+         # capture images from the webcam and process and detect the face
+        cam = cv2.VideoCapture(0)
+        id = request.POST['userId']
+        resMsg.format(id)
+        # Creating a cascade Image classifier
+        faceDetect = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        samples = 0
+        # Capturing the faces one by one and detect the faces and showing it on the window
+        while(True):
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+            for(x,y,w,h) in faces:
+                samples = samples + 1
+                # Saving the image dataset, only the face part
+                cv2.imwrite(BASE_DIR+'/ml/dataset/user.'+str(id)+'.'+str(samples)+'.jpg', gray[y:y+h,x:x+w])
+                cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
+                cv2.waitKey(250)
+            cv2.imshow("Face",img)
+            cv2.waitKey(1)
+            if(samples>5):
+                break
+    except MultiValueDictKeyError as e:
+        resStatus = status.HTTP_400_BAD_REQUEST
+        resMsg = "userId Parameter Missing."
+    except:
+        resStatus = status.HTTP_500_INTERNAL_SERVER_ERROR
+        resMsg = "Exception Occurred in create dataset!"
+    finally:
+        if(cam != None):
+            cam.release()
+        cv2.destroyAllWindows()
+    return Response(resMsg, status=resStatus)
