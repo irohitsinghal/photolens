bc8dc947db081110602f5f5a955e0953683f45df
Rohit Singhal <me.rohitsinghal@gmail.com>
2019-01-30 19:12:34 +0530

    Working live facial scan

--- lens/views.py
+++ lens/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import cv2
+
+from api.settings import BASE_DIR
 
 # Create your views here.
+
+
+def create_dataset(request, version):
+    userId = request.POST['userId']
+    print(cv2.__version__)
+    # Detect Face
+    # Creating a cascade Image classifier
+    faceDetect = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    # camture images from the webcam and process and detect the face
+    cam = cv2.VideoCapture(0)
+
+    # Our identifier
+    id = userId
+    # Our dataset naming counter
+    sampleNum = 0
+    # Capturing the faces one by one and detect the faces and showing it on the window
+    while(True):
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+        for(x,y,w,h) in faces:
+            # Whenever the program captures the face, we will write that is a folder
+            # Before capturing the face, we need to tell the script whose face it is
+            # For that we will need an identifier, here we call it id
+            # So now we captured a face, we need to write it in a file
+            sampleNum = sampleNum+1
+            # Saving the image dataset, but only the face part, cropping the rest
+            cv2.imwrite(BASE_DIR+'/ml/dataset/user.'+str(id)+'.'+str(sampleNum)+'.jpg', gray[y:y+h,x:x+w])
+            # @params the initial point of the rectangle will be x,y and
+            # @params end point will be x+width and y+height
+            # @params along with color of the rectangle
+            # @params thickness of the rectangle
+            cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
+            # Before continuing to the next loop, I want to give it a little pause
+            # waitKey of 100 millisecond
+            cv2.waitKey(250)
+        cv2.imshow("Face",img)
+        cv2.waitKey(1)
+        if(sampleNum>5):
+            break
+
+    #releasing the cam
+    cam.release()
+    # destroying all the windows
+    cv2.destroyAllWindows()
+
+    return redirect('/')
